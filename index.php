440c351b8598bb91650bedc8967b93caef5af03a
Captaincrogs <2003829@talnet.nl>
2019-12-03 16:25:24 +0100

    Uitwerking van de exercise

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<head>
+  <title> </title>
+  <meta charset="UTF-8">
+</head>
+
+<body>
+  <?php
+  echo "Hello world";
+  ?>
+
+
+</body>
+
+
+<footer>
+
+</footer>
+
+
+
+
+</html>
